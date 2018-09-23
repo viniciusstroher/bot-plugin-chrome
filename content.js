d4a6ef2b80bc66890e21833665f2413d44716589
unknown <viniciusferreirawk@gmail.com>
2018-09-23 02:12:13 -0300

    fix

--- content.js
+++ content.js
@@ -3,10 +3,10 @@
 //   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 // }
 var pluginName = "PLUGIN WATZ";
-console.log(pluginName,document.querySelector("body"));
-console.log(pluginName,$("body").html());
+// console.log(pluginName,document.querySelector("body"));
+// console.log(pluginName,$("body").html());
 
-
+console.log("START PLUGIN");
 function simulateMouseEvents(element, eventName) {
     var mouseEvent= document.createEvent ('MouseEvents');
     mouseEvent.initEvent (eventName, true, true);
@@ -14,22 +14,27 @@ function simulateMouseEvents(element, eventName) {
 }
 
 
-checkAlive = function () {
+function checkAlive() {
 	// body...
 	// console.log(pluginName,"ESTA VIVO?",$(":contains(Digite uma mensagem)"));
 
 	var contatosDom = document.querySelectorAll("[tabindex]")[2];
 	var imgDom 	    = $(contatosDom).find("img")[0];
-	// console.log(contatosDom);
+	console.log('contatosDom',contatosDom);
+	console.log('imgDom',imgDom);
+
 	if(contatosDom){
 		// console.log(imgDom);
 		// imgDom.dispatchEvent(new Event("click"));
-		simulateMouseEvents(imgDom, 'mousedown')
+		if(imgDom != null){
+			simulateMouseEvents(imgDom, 'mousedown');
+		}
+		
 
 	}
 	
 	// $("[tabindex] div:eq(0)").trigger("click");
-};
+}
 
 
 setInterval(checkAlive,1000);
