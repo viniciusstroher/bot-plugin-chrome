08e42ecced92c8949c47294ecee4c456d9a3f1ee
Stroher <vinicius.stroher@rcxit.com.br>
2018-10-10 13:30:08 -0300

    fix

--- bg.js
+++ bg.js
@@ -6,8 +6,26 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     }
 
     if(request.type == "save"){
-    	chrome.downloads.download({url:request.options.src,filename:request.options.src.split("/").pop()}, function(r){
-    		console.log('chrome.downloads:',r);
-    	});
+    	// chrome.downloads.download({url:request.options.src,filename:request.options.src.split("/").pop()}, function(r){
+    	// 	console.log('chrome.downloads:',r);
+    	// });
+
+    	var xmlhttp = null;
+	    if (window.XMLHttpRequest) {
+	        xmlhttp = new XMLHttpRequest();
+	    } else {
+	        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	    }
+
+	    var url    	   = "http://localhost:8093/files";
+	    var params 	   = "url="+request.options.src;
+	  	xmlhttp.onload = function(e){
+	  		console.log('onload',e);
+	  	};
+
+	    xmlhttp.open("POST", url, true);
+	    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	    xmlhttp.send(params);
+
     }
 });
